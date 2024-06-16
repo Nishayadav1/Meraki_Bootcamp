c3909631089a7c15baa43ae4742d912684ba9d78
Nishayadav1 <nishayadav22@navgurukul.org>
2024-06-16 16:07:41 +0530

    useState hook, and button

--- react/react-new/src/App.js
+++ react/react-new/src/App.js
@@ -1,9 +1,30 @@
 // import logo from './logo.svg';
-import React from "react"
+import React, { useState } from "react"
 import './App.css';
 import Home from "./components/Home";
 import User from "./components/User";
+
+
+
+
 function App() {
+  const [name,setName]=useState("Nisha")
+  const [num,setNum]=useState(0)
+  // let name="nisha";
+
+  function button(){
+    // name="kajal"
+    // console.log("my name is ",name);
+
+    setName("kajal");
+    console.log("hgfuieicuifgei")
+    setNum(num+1)
+
+    // return(
+    //   // console.log("this is a button")
+    //   alert("butoon function")
+    // )
+  }
   return (
     <>
     <Home/>
@@ -13,6 +34,20 @@ function App() {
       <li>banana</li>
       <li>orange</li>
     </ul>
+
+
+
+    {/* <button onClick={button}> Click</button> */}
+   {/* second way with funtion prenthisis by arrow function */}
+    {/* <button onClick={()=>button()}> Click1</button>  */}
+
+    {/* third way by direct console */}
+    {/* <button onClick={()=>console.log("I am nisha")}> Click2</button> */}
+    
+    
+    <h1>{name}</h1>
+    <h1>{num}</h1>
+    <button onClick={button}> Click</button>
     </>
   );
 }
